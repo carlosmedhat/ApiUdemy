809dfb55d8737fdf06010f2ce0b3251f7eee123a
carlosmedhat <kirolosmedhat274@gmail.com>
2023-07-15 15:11:59 +0300

    update

---
+++ app/src/main/java/com/example/apiudemy/MainActivity2.kt
@@ -0,0 +1,10 @@
+package com.example.apiudemy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
